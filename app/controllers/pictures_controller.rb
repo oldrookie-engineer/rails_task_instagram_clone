f346dc7af50c99f15e626ccfb9c89dafc714f1c5
oldrookie-engineer <paburofu0023@icloud.com>
2020-04-25 22:16:15 +0900

    pictureのCRUD機能を搭載

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,68 @@
+class PicturesController < ApplicationController
+  before_action :set_picture, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pictures = Picture.all
+  end
+
+  def show
+  end
+
+  def new
+    if params[:back]
+      @picture = Picture.new(picture_params)
+    else
+      @picture = Picture.new
+    end
+  end
+
+  def edit
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+
+    respond_to do |format|
+      if @picture.save
+        format.html { redirect_to @picture, notice: '投稿作成しました！' }
+        format.json { render :show, status: :created, location: @picture }
+      else
+        format.html { render :new }
+        format.json { render json: @picture.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @picture.update(picture_params)
+        format.html { redirect_to @picture, notice: '投稿編集しました！' }
+        format.json { render :show, status: :ok, location: @picture }
+      else
+        format.html { render :edit }
+        format.json { render json: @picture.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @picture.destroy
+    respond_to do |format|
+      format.html { redirect_to pictures_url, notice: '投稿削除しました！' }
+      format.json { head :no_content }
+    end
+  end
+
+  def confirm
+    @picture = Picture.new(picture_params)
+  end
+
+  private
+    def set_picture
+      @picture = Picture.find(params[:id])
+    end
+
+    def picture_params
+      params.require(:picture).permit(:image, :image_cache)
+    end
+end
