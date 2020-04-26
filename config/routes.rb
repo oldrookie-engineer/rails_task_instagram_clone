3fb9abe578cd6dfdbe999ecaf057ee0af8fd5681
oldrookie-engineer <paburofu0023@icloud.com>
2020-04-26 14:34:45 +0900

    ログイン機能を搭載

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :pictures do
     collection do
-      post :confirm 
+      post :confirm
     end
   end
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
